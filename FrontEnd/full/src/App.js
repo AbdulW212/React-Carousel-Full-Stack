b971454120f5b35a9c9dd48cffc81535b8473647
Abdul Waggeh <abudlayw@gmail.com>
2024-05-21 11:54:47 -0400

    full

---
+++ FrontEnd/full/src/App.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import { getNotes } from './util/notes-api';
+import NotesPage from './pages/NotesPage';
+import CreateForm from './components/CreateForm';
+import UpdateForm from './components/UpdateForm';
+import './App.css';
+
+export default function App() {
+  const [notes, setNotes] = useState([]);
+  const [updateForm, setUpdateForm] = useState({ _id: null, title: '', body: '' });
+  
+  useEffect(() => {
+    getNotes(setNotes)
+  }, [notes])
+
+  return (
+    <div className='App'>
+      <CreateForm setNotes={setNotes} />
+      <UpdateForm updateForm={updateForm} setUpdateForm={setUpdateForm} />
+      <NotesPage notes={notes} setNotes={setNotes} setUpdateForm={setUpdateForm} />
+    </div>
+  );
+}
